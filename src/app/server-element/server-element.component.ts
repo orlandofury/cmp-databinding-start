0d5109572c78a5ce91037388656a1cb5f909a407
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-14 17:42:10 -0600

    View Encapsulation unless explit with encapsulation property in the Component decorator all css styles are particular for the component including the app component

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -4,7 +4,7 @@ import { Component,Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation:ViewEncapsulation.None
+  encapsulation:ViewEncapsulation.Emulated
 })
 export class ServerElementComponent {
   @Input('srvElement') element: {type: string,name:string,content:string};
