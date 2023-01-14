c70effa90aea9b659d4bed6d9300d4a3b1f1ebed
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-14 17:15:55 -0600

    Binding to custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type:'server',name:'testserver',content:'Just a Test!'}];
-  
+  onServerAdded(serverData: {serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
