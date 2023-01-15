0539cc22bc0e277b0a46b8e4945ca82bd2d7e9ad
Orlando Flores Silva <orlandofury@gmail.com>
2023-01-14 18:55:31 -0600

    Lifecycle Hoos in Action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,4 +22,10 @@ export class AppComponent {
       content: blueprintData.serverContent
     });
   }
+  onChangeFirst(){
+    this.serverElements[0].name = 'Changed';
+  }
+  onDestroyFirst(){
+    this.serverElements.splice(0,1);
+  }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component,Input, ViewEncapsulation } from '@angular/core';
+import { 
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input, 
+  OnChanges, 
+  OnDestroy, 
+  OnInit, 
+  SimpleChanges, 
+  ViewEncapsulation 
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,6 +19,48 @@ import { Component,Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation:ViewEncapsulation.Emulated
 })
-export class ServerElementComponent {
+export class ServerElementComponent implements 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
+  
   @Input('srvElement') element: {type: string,name:string,content:string};
+  @Input() name:string;
+  /**
+   *
+   */
+  constructor() {
+    console.log('constructor called!');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called!');
+  }
+  
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck called!');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called!');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called!');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called!');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called!');
+  }
 }
